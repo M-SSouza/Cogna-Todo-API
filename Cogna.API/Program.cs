fd8b6f40b9c1485083d91d81a752612f8e87dace
Matheus Souza <matheus.souza@grafftecnologia.com.br>
2022-04-01 19:35:04 -0300

    Adicionado comentarios do swagger.

--- Cogna.API/Program.cs
+++ Cogna.API/Program.cs
@@ -3,6 +3,7 @@ using Cogna.Core.Interfaces.Repositories;
 using Cogna.Infrastructure.Persistence;
 using Cogna.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +19,26 @@ builder.Services.AddScoped<ITodosService, TodosService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc(
+    "v1", new OpenApiInfo
+    {
+        Title = "Cogna API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Matheus .S Souza",
+            Email = "matheusdasilvasouza20@gmail.com",
+            Url = new Uri("https://www.linkedin.com/in/mdassouza/")
+        }
+    });
+
+    string xmlFile = "Cogna.API.xml";
+    string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    x.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
